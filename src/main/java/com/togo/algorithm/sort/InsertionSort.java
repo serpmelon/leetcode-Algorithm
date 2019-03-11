745a1412af9c4a0f9296526c89e758a2964f703a
serpmelon <tyn243222791@126.com>
2019-03-11 14:43:10 +0800

    插入排序实现

---
+++ src/main/java/com/togo/algorithm/sort/InsertionSort.java
@@ -0,0 +1,22 @@
+package com.togo.algorithm.sort;
+
+public class InsertionSort extends AbstractSort {
+
+	@Override
+	protected void sort(int[] nums) {
+
+		for (int i = 1; i < nums.length; i++) {
+
+			int pre = i - 1;
+			int current = nums[i];
+			while (pre >= 0 && nums[pre] > current) {
+
+				nums[pre + 1] = nums[pre];
+				pre--;
+			}
+
+			nums[pre + 1] = current;
+		}
+	}
+
+}
