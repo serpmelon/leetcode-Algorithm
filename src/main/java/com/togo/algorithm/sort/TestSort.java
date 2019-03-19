c44309847f33d48a1163c804acb2a1668539c02d
serpmelon <tyn243222791@126.com>
2019-03-19 14:33:04 +0800

    对照新方法做了相应修改

--- src/main/java/com/togo/algorithm/sort/TestSort.java
+++ src/main/java/com/togo/algorithm/sort/TestSort.java
@@ -9,29 +9,31 @@ public class TestSort {
 	public static void main(String[] args) {
 
 		int n = 100000;
-		int[] a = createArray(10, 0, 10);
+		int[] a = createArray(n, 0, 1000);
 
 		AbstractSort sort = new BubbleSort();
-		// sort.sortAndPrintArray(a.clone());
-		sort.sortTime(a.clone());
+		sort.sortAndPrint(a.clone());
 
 		sort = new InsertionSort();
-		// sort.sortAndPrintArray(a.clone());
-		sort.sortTime(a.clone());
+		sort.sortAndPrint(a.clone());
 
 		sort = new SelectionSort();
-		sort.sortAndPrintArray(a.clone());
+		sort.sortAndPrint(a.clone());
+
+		sort = new MergeSort();
+		sort.sortAndPrint(a.clone());
 	}
 
 	public static int[] createArray(int n, int min, int max) {
 
 		int[] a = new int[n];
-
-		System.out.println("create array :");
+		if (n <= 10)
+			System.out.println("create array :");
 		for (int i = 0; i < n; i++) {
 
 			a[i] = random.nextInt(max - min) + min;
-			System.out.print(a[i] + "  ");
+			if (n <= 10)
+				System.out.print(a[i] + " ");
 		}
 
 		System.out.println();
