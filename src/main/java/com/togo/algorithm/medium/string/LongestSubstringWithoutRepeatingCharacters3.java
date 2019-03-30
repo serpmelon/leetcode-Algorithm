a6abfba8c9f3217c4665ab5e06de9f1ac6ad7a7d
serpmelon <tyn243222791@126.com>
2019-03-30 22:30:56 +0800

    更新了两个使用滑动窗口解决的算法

--- src/main/java/com/togo/algorithm/medium/string/LongestSubstringWithoutRepeatingCharacters3.java
+++ src/main/java/com/togo/algorithm/medium/string/LongestSubstringWithoutRepeatingCharacters3.java
@@ -1,6 +1,8 @@
 package com.togo.algorithm.medium.string;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -93,9 +95,100 @@ public class LongestSubstringWithoutRepeatingCharacters3 {
 		return true;
 	}
 
+	/**
+	 * 
+	 * <p>
+	 * Method ：lengthOfLongestSubstring_1
+	 * <p>
+	 * Description :滑动窗口，[i,j) i为首，j为尾，j++，当出现重复时，i++，然后再判断是否重复，如果还重复则i++。
+	 *
+	 * @param s
+	 * @return
+	 * @author taiyn
+	 *         <p>
+	 *         --------------------------------------------------------------<br>
+	 *         修改履历：<br>
+	 *         <li>2019年3月30日，taiyn，创建方法；<br>
+	 *         --------------------------------------------------------------<br>
+	 *         </p>
+	 */
+	public int lengthOfLongestSubstring_1(String s) {
+
+		if (s == null || s.length() == 0)
+			return 0;
+
+		int max = 0, strLength = s.length();
+		if (strLength == 1)
+			return 1;
+		int i = 0, j = 0;
+
+		Set<Character> set = new HashSet<>();
+
+		while (i < strLength && j < strLength) {
+
+			// 集合包含则表示重复
+			if (set.contains(s.charAt(j))) {
+				// 删除i，并i++
+				set.remove(s.charAt(i++));
+			} else {
+				// 不重复则增加元素，并且j++
+				set.add(s.charAt(j++));
+				max = Math.max(max, j - i);
+			}
+		}
+
+		return max;
+	}
+
+	/**
+	 * 
+	 * <p>
+	 * Method ：lengthOfLongestSubstring
+	 * <p>
+	 * Description :优化的滑动窗口，如果有重复数据，则直接从重复数据之后在重新计算
+	 *
+	 * @param s
+	 * @return
+	 * @author taiyn
+	 *         <p>
+	 *         --------------------------------------------------------------<br>
+	 *         修改履历：<br>
+	 *         <li>2019年3月30日，taiyn，创建方法；<br>
+	 *         --------------------------------------------------------------<br>
+	 *         </p>
+	 */
+	public int lengthOfLongestSubstring_2(String s) {
+
+		if (s == null || s.length() == 0)
+			return 0;
+
+		int max = 0, strLength = s.length();
+		if (strLength == 1)
+			return 1;
+
+		int i = 0, j = 0;
+		// 将字符和位置存储在map中
+		Map<Character, Integer> map = new HashMap<>();
+		while (j < strLength) {
+
+			if (map.containsKey(s.charAt(j))) {
+
+				// 存在则充map中获取重复元素后面的位置
+				// 这里取max是因为i位置之前的元素重复的话是不关心的，所以重复了还是取现在的i值，即取最大的i值
+				i = Math.max(map.get(s.charAt(j)) + 1, i);
+			}
+
+			map.put(s.charAt(j), j);
+			max = Math.max(max, j - i + 1);
+			j++;
+		}
+
+		return max;
+	}
+
 	public static void main(String[] args) {
 
 		String a = "bb";
-//		System.out.println(lengthOfLongestSubstring(a));
+		// System.out.println(lengthOfLongestSubstring(a));
 	}
 }
