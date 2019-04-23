cfec9a0afa3417d5d783784fa70c2abb6545f044
serpmelon <tyn243222791@126.com>
2019-04-23 15:42:25 +0800

    加了排序思想和注释

--- src/main/java/com/togo/algorithm/sort/InsertionSort.java
+++ src/main/java/com/togo/algorithm/sort/InsertionSort.java
@@ -23,19 +23,34 @@ public class InsertionSort extends AbstractSort {
 		return "InsertionSort 插入排序";
 	}
 
+	/**
+	 * 
+	 * <p>
+	 * Method ：sort
+	 * <p>
+	 * Description :遍历元素，将元素从排好序的后面比较，在合适的地方插入元素
+	 *
+	 * @param nums
+	 * @see com.togo.algorithm.sort.AbstractSort#sort(int[])
+	 */
 	@Override
 	protected void sort(int[] nums) {
 
+		// 从1开始，也就是0位置算排好序了
 		for (int i = 1; i < nums.length; i++) {
 
+			// pre指向排好序的最后一位
 			int pre = i - 1;
 			int current = nums[i];
+			// 如果是从大到小，则nums[pre] < current
 			while (pre >= 0 && nums[pre] > current) {
 
+				// 这一步是将pre的值向后移动了一步，pre位置上还是pre的值，这个值会在最后替换成current
 				nums[pre + 1] = nums[pre];
 				pre--;
 			}
 
+			// 替换
 			nums[pre + 1] = current;
 		}
 	}
--- src/main/java/com/togo/algorithm/sort/SelectionSort.java
+++ src/main/java/com/togo/algorithm/sort/SelectionSort.java
@@ -23,19 +23,32 @@ public class SelectionSort extends AbstractSort {
 		return "SelectionSort 选择排序";
 	}
 
+	/**
+	 * 
+	 * <p>
+	 * Method ：sort
+	 * <p>
+	 * Description :每次遍历寻找当前最小值，然后把最小值放到前边
+	 *
+	 * @param nums
+	 * @see com.togo.algorithm.sort.AbstractSort#sort(int[])
+	 */
 	@Override
 	protected void sort(int[] nums) {
 
 		for (int i = 0; i < nums.length; i++) {
 
+			// 默认每次遍历的第一个为最小值
 			int minIndex = i;
 			for (int j = i + 1; j < nums.length; j++) {
 
+				// 遍历寻找最小值
 				if (nums[j] < nums[minIndex]) {
 					minIndex = j;
 				}
 			}
 
+			// 找到后 交换
 			int temp = nums[minIndex];
 			nums[minIndex] = nums[i];
 			nums[i] = temp;
