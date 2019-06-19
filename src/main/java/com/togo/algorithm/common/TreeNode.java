e3f861578aa7f26b03b4d489914322587b79f5ff
taiyn <taiyn@ziroom.com>
2019-06-19 08:56:08 +0800

    二叉树锯齿遍历103

---
+++ src/main/java/com/togo/algorithm/common/TreeNode.java
@@ -0,0 +1,26 @@
+package com.togo.algorithm.common;
+
+/**
+ * <p></p>
+ * <p>
+ * <PRE>
+ * <BR>    修改记录
+ * <BR>-----------------------------------------------
+ * <BR>    修改日期         修改人          修改内容
+ * </PRE>
+ *
+ * @author taiyn
+ * @version 1.0
+ * @date Created in 2019年06月19日 08:52
+ * @since 1.0
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
