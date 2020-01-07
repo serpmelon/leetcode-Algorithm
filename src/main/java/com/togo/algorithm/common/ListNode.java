8e41c98608261ec29841e8ba6050a957447e8cb3
serpmelon <tyn243222791@126.com>
2020-01-07 08:51:02 +0800

    公共类

---
+++ src/main/java/com/togo/algorithm/common/ListNode.java
@@ -0,0 +1,26 @@
+package com.togo.algorithm.common;
+
+
+/**
+ * <p></p>
+ * <p>
+ * <PRE>
+ * <BR>    修改记录
+ * <BR>-----------------------------------------------
+ * <BR>    修改日期         修改人          修改内容
+ * </PRE>
+ *
+ * @author taiyn
+ * @version 1.0
+ * @date Created in 2020年01月07日 08:50
+ * @since 1.0
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
